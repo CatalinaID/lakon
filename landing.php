f7d5a09e68738c3be4044a32eda8440fbbe986f0
Hayyu hanifah <heripottypot@gmail.com>
2015-12-05 17:25:52 +0700

    init layout landing

---
+++ landing.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+	    <meta name="description" content="">
+	    <meta name="author" content="">
+
+	    <title>LAKON</title>
+
+	    <!-- Bootstrap Core CSS -->
+	    <link href="css/bootstrap.min.css" rel="stylesheet">
+	    <link href="css/style.css" rel="stylesheet">
+
+	    <!-- Custom CSS -->
+	    <style>
+	    body {
+	        padding-top: 70px;
+	        /* Required padding for .navbar-fixed-top. Remove if using .navbar-static-top. Change if height of navigation changes. */
+	    }
+	    </style>
+	</head>
+	<body>
+		<div class="container">
+			<div class="col-lg-12">
+				<img src="bs.png" class="img-responsive" style="margin: 0 auto;">
+			</div>
+		</div>
+
+	</body>
+</html>
